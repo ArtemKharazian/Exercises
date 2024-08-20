efed3b302e589eb349c3c07e7064d72d6664e97b
ArtemKharazian <artem.kharazian@accenture.com>
2024-08-20 16:42:57 +0200

    Data structures: objects and arrays

--- exercises.js
+++ exercises.js
@@ -57,7 +57,7 @@ console.log(min(10, 5));
 
 //aufgabe Recursion
 
-function isEvent(n) {
+  function isEvent(n) {
     if (n === 0) {
       return "Ваше число чётное";
     } else if (n === 1) {
@@ -96,4 +96,101 @@ function isEvent(n) {
   
   console.log(countBs("Barbeque"));
   console.log(countChar("spectrum", "s"));
+
+  //aufgabe Sum of the range
+
+  function range (first, last) {
+    var result = [];
+    for (var i = first; i <= last; i++) {
+      result.push(i);
+    }
+    return result;
+  }
+  
+  console.log(range(1, 10));
+  
+  function sum(result) {
+    var total = 0;
+    for (j = 0; j < result.length; j++) {
+      total += result[j];
+    }
+     return total;  //bonus habe ich nicht verstanden.
+  }
+  
+  console.log(sum(range(1, 10)));
+
+  // aufgabe Reversing the array
+
+  function reverseArray (myArray) {
+    return myArray.reverse(); 
+  }
+  
+  console.log(reverseArray([1, 2, 3, 4, 5]));
+
+  //oder 
+
+  function reverseArray(array) {
+    let newArray = [];
+    for (let i = array.length - 1; i >= 0; i--) {
+        newArray.push(array[i]);
+    }
+    return newArray;
+    }
+
+console.log(reverseArray([1, 2, 3, 4, 5]));
+
+//function reverseArrayInPlace habe ich nicht vertanden.
+
+// aufgabe List
+
+function ArrayToList (array) {
+    let list = null;
+    for(let i = array.length - 1; i >= 0; i--) {
+      list = { value: array[i], rest: list };
+    }  
+    return list;
+  }
+  
+  console.log (ArrayToList([1, 2, 3]));
+
+  function listToArray (list) {
+    let array = [];
+    while (list !== null) {
+      array.push(list.value);
+      list = list.rest;
+      }
+      return array;
+    }
+  
+  console.log(listToArray(ArrayToList([1, 2, 3])));
+
+// prepent und nth habe ich nicht verstanden.
+
+// aufgabe Deep comparison habe ich auch nicht verstanden.
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
